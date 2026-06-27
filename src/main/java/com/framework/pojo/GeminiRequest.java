a5f04cdc5fd4d792533ad2fc89426ecf4fbe56e4
Suresh.Patibandla <venkatasurya0@gmail.com>
2026-06-27 23:07:27 +0530

    added google gemini integration code

---
+++ src/main/java/com/framework/pojo/GeminiRequest.java
@@ -0,0 +1,40 @@
+package com.framework.pojo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GeminiRequest {
+
+    private List<Content> contents;
+    private GenerationConfig generationConfig;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Content {
+        private List<Part> parts;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Part {
+        private String text;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GenerationConfig {
+        private double temperature;
+        private int maxOutputTokens;
+    }
+}
